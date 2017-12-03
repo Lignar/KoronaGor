8501062456ae75418f541333a3c12ef64ab788a0
janusz <januszlignarski@o2.pl>
2017-12-03 01:31:33 +0100

    Initial commit

---
+++ app/src/main/java/pl/edu/pwr/jlignarski/koronagor/MenuActivity.java
@@ -0,0 +1,16 @@
+package pl.edu.pwr.jlignarski.koronagor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
